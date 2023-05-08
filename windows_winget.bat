55bd9c4868d8702d14c8ce5cfd5b70e2026f47af
Mateus Bucioli Marcusso <mateusbuc@gmail.com>
2023-05-08 00:11:02 -0300

    winget apps

---
+++ windows_winget.bat
@@ -0,0 +1,7 @@
+winget install 7zip.7zip
+winget install Starship.Starship
+winget install GoLang.Go
+winget install Git.Git
+winget install Logseq.Logseq
+winget install Microsoft.PowerShell
+winget install Microsoft.VisualStudioCode
