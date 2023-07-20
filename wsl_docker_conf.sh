a3f006469ac8e98be5d327c867401bc9fd54b29e
Mateus Bucioli <48491077+MateusBucioli@users.noreply.github.com>
2023-07-20 12:12:23 -0300

    Update wsl_docker_conf.sh

--- wsl_docker_conf.sh
+++ wsl_docker_conf.sh
@@ -29,11 +29,25 @@ sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo service docker start
 
-# Reinicie o wsl
-
-# Para testar a instalação utilize os comandos abaixo
-# sudo docker run hello-world
+# Para testar a instalação utilize o comando abaixo
+  # sudo docker run hello-world
 
 # Caso queira instale o portainer https://docs.portainer.io/start/install-ce/server/docker/wsl
-# docker volume create portainer_data
-# docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+  # docker volume create portainer_data
+  # docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# Configurando docker sem sudo
+# Execute o comando `sudo visudo`
+# No arquivo aberto insira `<USUARIO> ALL=(ALL) NOPASSWD: /usr/bin/dockerd` alterando <USUARIO> para seu usuario do linux
+# Salve e feche o arquivo
+# Execute o comando `sudo usermod -a -G docker $USER`
+# Reinicie o wsl (saindo com exit e no Windows execute `wsl --shutdown`
+
+# Caso queira que o Docker inicie com o wsl, execute as instrucoes abaixo (caso esteja usando zsh, troque o bashrc por zshrc)
+  # echo  >> ~/.bashrc  
+  # echo '# Start Docker daemon automatically when logging in if not running.' >> ~/.bashrc
+  # echo 'RUNNING=`ps aux | grep dockerd | grep -v grep`' >> ~/.bashrc
+  # echo 'if [ -z "$RUNNING" ]; then' >> ~/.bashrc
+  # echo '    sudo dockerd > /dev/null 2>&1 &' >> ~/.bashrc
+  # echo '    disown' >> ~/.bashrc
+  # echo 'fi' >> ~/.bashrc
