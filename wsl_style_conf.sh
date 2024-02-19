a5210916def8cfd51cf4bb428f18b7f02ef45e81
Mateus Bucioli <48491077+MateusBucioli@users.noreply.github.com>
2024-02-19 17:53:47 -0300

    Update wsl_style_conf.sh

--- wsl_style_conf.sh
+++ wsl_style_conf.sh
@@ -1,6 +1,10 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install zsh -y
 sudo apt install unzip -y
+sudo apt remove git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update -y
+sudo apt install git -y
 
 git config --global core.autocrlf true
 
