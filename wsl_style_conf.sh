d397259854c5bf4f54bde0daf4ed2b5bb8d8ee26
Mateus Bucioli <48491077+MateusBucioli@users.noreply.github.com>
2024-02-19 17:15:58 -0300

    Update wsl_style_conf.sh

--- wsl_style_conf.sh
+++ wsl_style_conf.sh
@@ -1,24 +1,16 @@
 sudo apt update -y && sudo apt upgrade -y
-
 sudo apt install zsh -y
-
 sudo apt install unzip -y
 
-curl -sS https://starship.rs/install.sh | sh
-
-printf 'eval "$(starship init zsh)"\n' >> ~/.zshrc
-
-printf 'zsh\n' >> ~/.bashrc
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git config --global core.autocrlf true
 
 wget -x https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip -O /tmp/FiraCode.zip
-
 sudo unzip /tmp/FiraCode.zip -d /usr/share/fonts/ && sudo rm /tmp/FiraCode.zip
 
+printf 'zsh\n' >> ~/.bashrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 sed -r '/plugins=\(/ s/(^.*)(=.*)/\1=\(git golang docker docker-compose zsh-autosuggestions zsh-syntax-highlighting\)/g' -i ~/.zshrc
 
-git config --global core.autocrlf true
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
