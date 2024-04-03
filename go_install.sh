a8b837bf3fa0f384d105647c70269306e7112c51
Mateus Bucioli <48491077+MateusBucioli@users.noreply.github.com>
2024-04-03 11:11:09 -0300

    Update go_install.sh

--- go_install.sh
+++ go_install.sh
@@ -2,14 +2,29 @@ url=$(curl -s https://go.dev/dl/ | grep -oP 'go[0-9\.]+linux-amd64.tar.gz' | hea
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/golang.tar.gz
 sudo rm /tmp/golang.tar.gz
 
-if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.zshrc; then
-    echo "Adding Go to PATH in ~/.zshrc..."
-    echo "# Add go to PATH" >> ~/.zshrc
-    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
+fish_config="$HOME/.config/fish/config.fish"
+if [ -f "$fish_config" ]; then
+    if ! grep -q "set -gx PATH /usr/local/go/bin \$PATH" $fish_config; then
+        echo "Adding Go to PATH in $fish_config..."
+        echo "" >> $fish_config
+        echo "# Add Go to PATH" >> $fish_config
+        echo "set -gx PATH /usr/local/go/bin \$PATH" >> $fish_config && echo "Path added successfully in $fish_config"
+        echo "Run 'source $fish_config' to apply changes"
+    else
+        echo "Go path already configured in $fish_config"
+    fi
 else
-  echo "Go path already configured in ~/.zshrc"
+    zsh_config="$HOME/.zshrc"
+    if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" $zsh_config; then
+        echo "Adding Go to PATH in $zsh_config..."
+        echo "" >> $fish_config
+        echo "# Add Go to PATH" >> $zsh_config
+        echo "export PATH=\$PATH:/usr/local/go/bin" >> $zsh_config
+        echo "Run 'source $zsh_config' to apply changes"
+    else
+    echo "Go path already configured in $zsh_config"
+    fi
 fi
 
 echo ""
 echo "Go installed successfully"
-echo "Run 'source ~/.zshrc' to apply changes"
